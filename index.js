08b92dfc8f7c02734078ece98d01c1b8c2df4aa6
dineshyadav19 <dinesh.2108.yadav@gmail.com>
2023-03-26 13:05:28 +0530

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+const captcha = document.getElementById('captcha');
+const captchaInput = document.getElementById('captcha-input');
+const characters =
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const captchaLength = 6;
+let captchaText = '';
+
+for (let i = 0; i < captchaLength; i++) {
+  captchaText += characters.charAt(
+    Math.floor(Math.random() * characters.length)
+  );
+}
+
+captcha.textContent = captchaText;
+
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (captchaInput.value === captchaText) {
+    alert('Form submitted successfully!');
+  } else {
+    alert('Invalid captcha, please try again.');
+    captchaText = '';
+    for (let i = 0; i < captchaLength; i++) {
+      captchaText += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+    }
+    captcha.textContent = captchaText;
+  }
+});
